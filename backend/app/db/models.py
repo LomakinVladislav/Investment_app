bc48d882292640d7cf2ef6431ff71650b506077a
Vlad <winr2005@gmail.com>
2025-01-13 13:52:12 +0500

    Созданы основные таблицы в БД, успешно выполнена тестовая вставка данных

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,10 +1,24 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 metadata_obj = MetaData()
 
-user_table = Table(
-    "user",
+users_table = Table(
+    "users",
     metadata_obj,
     Column("id", Integer, primary_key=True),
-    Column("username", String)
+    Column("name", String),
+)
+
+assets_table = Table(
+    "assets",
+    metadata_obj,
+    Column("user_id", Integer, ForeignKey('users.id'), primary_key=True),
+    Column("currency_id", Integer, ForeignKey('cryptocurrencies.id'), primary_key=True),
+)
+
+cryptocurrencies_table = Table(
+    "cryptocurrencies",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String),
 )
--- backend/app/db/queries/core.py
+++ backend/app/db/queries/core.py
@@ -1,10 +1,24 @@
 # Файл с описанием функций (методов) для создания запросов и команд базе данных
-
+from sqlalchemy import text, insert
 import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from db.models import metadata_obj
+from db.models import metadata_obj, users_table
 from db.database import sync_engine
 
 def create_tables():
+    sync_engine.echo = False
+    metadata_obj.drop_all(sync_engine)
     metadata_obj.create_all(sync_engine)
+    sync_engine.echo = True
+
+def insert_data():
+    with sync_engine.connect() as conn:
+        stmt = insert(users_table).values(
+            [
+                {"name": "Satoshi"},
+                {"name": "Elon"}
+            ]
+        )
+        conn.execute(stmt)
+        conn.commit()
